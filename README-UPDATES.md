f69e9b66d7d493967f55d374aa91af5fd3a07d2f
cola <csin120@wgu.edu>
2025-01-20 16:31:20 -0500

    Part E - sample inventory is created with 5 parts and 5 products

--- README-UPDATES.md
+++ README-UPDATES.md
@@ -1,3 +1,5 @@
+
+//////////
 Part C
 mainscreen.html Line 19 - Changed project name
 
@@ -12,16 +14,35 @@ ProductForm.html
 
 Created pages - about.html, forms.css, about.css, mainscreen.css,AboutMeController
 Created folders - static.images
+
+
+///////////
 Part D
+about us page is completed with description and a link to mainscreen.html.
+mainscreen.html - line 33 house line to about.html
 
+
+//////////
 Part E
+bootstrapData.java - updated lines 40 - 133 with 5 parts and 5 products. 
+program does not overwrite data in DB
 
+
+//////////
 Part F
 
+
+//////////
 Part G
 
+
+//////////
 Part H
 
+
+//////////
 Part I
 
+
+//////////
 Part J
--- src/main/java/com/example/demo/bootstrap/BootStrapData.java
+++ src/main/java/com/example/demo/bootstrap/BootStrapData.java
@@ -1,20 +1,16 @@
 package com.example.demo.bootstrap;
 
+import com.example.demo.domain.InhousePart;
 import com.example.demo.domain.OutsourcedPart;
-import com.example.demo.domain.Part;
 import com.example.demo.domain.Product;
+import com.example.demo.repositories.InhousePartRepository;
 import com.example.demo.repositories.OutsourcedPartRepository;
 import com.example.demo.repositories.PartRepository;
 import com.example.demo.repositories.ProductRepository;
-import com.example.demo.service.OutsourcedPartService;
-import com.example.demo.service.OutsourcedPartServiceImpl;
-import com.example.demo.service.ProductService;
-import com.example.demo.service.ProductServiceImpl;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.Optional;
 
 /**
  *
@@ -29,15 +25,115 @@ public class BootStrapData implements CommandLineRunner {
     private final ProductRepository productRepository;
 
     private final OutsourcedPartRepository outsourcedPartRepository;
+    private final InhousePartRepository inhousePartRepository;
 
-    public BootStrapData(PartRepository partRepository, ProductRepository productRepository, OutsourcedPartRepository outsourcedPartRepository) {
+    public BootStrapData(PartRepository partRepository, ProductRepository productRepository, OutsourcedPartRepository outsourcedPartRepository, InhousePartRepository inhousePartRepository) {
         this.partRepository = partRepository;
         this.productRepository = productRepository;
         this.outsourcedPartRepository=outsourcedPartRepository;
+        this.inhousePartRepository = inhousePartRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        //Cola's block start
+        if(inhousePartRepository.count()==0){
+
+
+
+        //parts
+
+        InhousePart chair = new InhousePart();
+
+        chair.setName("Arm Chair");
+        chair.setInv(50);
+        chair.setPrice(200.00);
+        chair.setId(1);
+        inhousePartRepository.save(chair);
+
+        InhousePart rug =new InhousePart();
+
+        rug.setName("Rug");
+        rug.setInv(50);
+        rug.setPrice(100.00);
+        rug.setId(2);
+        inhousePartRepository.save(rug);
+
+        InhousePart lamp =new InhousePart();
+
+        lamp.setName("Lamp");
+        lamp.setInv(50);
+        lamp.setPrice(50.00);
+        lamp.setId(3);
+        inhousePartRepository.save(lamp);
+
+        }
+
+        if(outsourcedPartRepository.count()==0){
+        OutsourcedPart coffeeTable = new OutsourcedPart();
+
+        coffeeTable.setName("Coffee Table");
+        coffeeTable.setInv(50);
+        coffeeTable.setPrice(100.00);
+        coffeeTable.setId(4);
+        outsourcedPartRepository.save(coffeeTable);
+
+        OutsourcedPart mirror = new OutsourcedPart();
+
+        mirror.setName("Mirror");
+        mirror.setInv(50);
+        mirror.setPrice(50.00);
+        mirror.setId(5);
+        outsourcedPartRepository.save(mirror);
+        }
+
+
+        //products
+
+        if(productRepository.count()==0){
+
+            Product livingRoomSet = new Product();
+            livingRoomSet.setName("Lving Room Set");
+            livingRoomSet.setInv(10);
+            livingRoomSet.setPrice(500.00);
+            livingRoomSet.setId(101);
+            productRepository.save(livingRoomSet);
+
+            Product bathroomSet = new Product();
+            bathroomSet.setName("Bathroom Set");
+            bathroomSet.setInv(10);
+            bathroomSet.setPrice(500.00);
+            bathroomSet.setId(102);
+            productRepository.save(bathroomSet);
+
+            Product bedroomSet = new Product();
+            bedroomSet.setName("Bedroom Set");
+            bedroomSet.setInv(10);
+            bedroomSet.setPrice(500.00);
+            bedroomSet.setId(103);
+            productRepository.save(bedroomSet);
+
+            Product kitchenSet = new Product();
+            kitchenSet.setName("Kitchen Set");
+            kitchenSet.setInv(10);
+            kitchenSet.setPrice(500.00);
+            kitchenSet.setId(104);
+            productRepository.save(kitchenSet);
+
+            Product diningRoomSet = new Product();
+            diningRoomSet.setName("Dining Room Set");
+            diningRoomSet.setInv(10);
+            diningRoomSet.setPrice(500.00);
+            diningRoomSet.setId(105);
+            productRepository.save(diningRoomSet);
+
+        }
+
+        //Cola's block end
+
+
+
 
        /*
         OutsourcedPart o= new OutsourcedPart();
