61fe5a29441c7580d66c895a1c1410a5b663f7d8
cola <csin120@wgu.edu>
2025-01-25 13:09:30 -0500

    Part H - Displays error message when inventory value is greater than max and min

--- src/main/java/com/example/demo/validators/InventoryValidator.java
+++ src/main/java/com/example/demo/validators/InventoryValidator.java
@@ -6,6 +6,7 @@ import com.example.demo.service.ProductService;
 import com.example.demo.service.ProductServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
@@ -16,7 +17,8 @@ import javax.validation.ConstraintValidatorContext;
  *
  *
  */
-public class InventoryValidator implements ConstraintValidator<ValidInventory, Part> {
+
+public class InventoryValidator implements ConstraintValidator<ValidInventory, Object> {
     @Autowired
     private ApplicationContext context;
     public static  ApplicationContext myContext;
@@ -25,22 +27,34 @@ public class InventoryValidator implements ConstraintValidator<ValidInventory, P
         //ConstraintValidator.super.initialize(constraintAnnotation);
     }
 
-    @Override
-    public boolean isValid(Part part, ConstraintValidatorContext constraintValidatorContext) {
-        //Cola's code block start
 
-        if (part.getInv() > part.getMaxInv()) {
-            constraintValidatorContext.buildConstraintViolationWithTemplate("Solution: Inventory is greater than max inventory. Please make it lower.").addConstraintViolation();
-            return false;
-        } else if (part.getInv() < part.getMinInv()) {
-            constraintValidatorContext.buildConstraintViolationWithTemplate("Solution: Please add value greater than zero.").addConstraintViolation();
-            return false;
 
+    //Cola's block start
+        @Override
+        public boolean isValid(Object object, ConstraintValidatorContext context) {
+            if (object instanceof Part part) {
+                return validateInventory(part.getInv(), part.getMinInv(), part.getMaxInv(), context);
+            } else if (object instanceof Product product) {
+                return validateInventory(product.getInv(), product.getMinInv(), product.getMaxInv(), context);
+            }
+            return true; // Default to valid for unsupported types
         }
 
-        //Cola's block end
-        return true;
+    private boolean validateInventory(int inv, int minInv, int maxInv, ConstraintValidatorContext context) {
+        if (inv > maxInv) {
+            context.buildConstraintViolationWithTemplate("Solution: Inventory is greater than max inventory. Please make it lower.")
+                    .addConstraintViolation();
+            return false;
+        } else if (inv < minInv) {
+            context.buildConstraintViolationWithTemplate("Solution: Please add value greater than zero.")
+                    .addConstraintViolation();
+            return false;
+        }
 
+
+
+        return true;
+//Cola's block end
     }
 }
 
--- src/main/java/com/example/demo/validators/ValidInventory.java
+++ src/main/java/com/example/demo/validators/ValidInventory.java
@@ -2,10 +2,7 @@ package com.example.demo.validators;
 
 import javax.validation.Constraint;
 import javax.validation.Payload;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import java.lang.annotation.*;
 
 /**
  *
@@ -16,6 +13,7 @@ import java.lang.annotation.Target;
 @Constraint(validatedBy = {InventoryValidator.class})
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
+
 public @interface ValidInventory {
     String message() default "Inventory Error!";
     Class<?> [] groups() default {};
